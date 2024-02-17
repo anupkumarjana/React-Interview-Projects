bd044fee6c7ad2b3f9d8e8e928dc12d309385f72
anupkumarjana <janaanup7872@gmail.com>
2024-02-17 17:28:47 +0530

    compled multi-selector-search-bar completed

--- selector-search-bar/src/components/Pills.jsx
+++ selector-search-bar/src/components/Pills.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function Pills() {
-  return <div>Pills</div>;
+function Pills({ image, firstName, lastName, onClick }) {
+  return (
+    <span
+      onClick={onClick}
+      className="flex gap-2 items-center rounded-3xl bg-blue-700 text-white p-2 cursor-pointer"
+    >
+      <img src={image} alt="" className="w-6 h-6 rounded-full" />
+      <span>
+        {firstName} {lastName} &times;
+      </span>
+    </span>
+  );
 }
 
 export default Pills;
