3eef0c83af25022bb61bb0fb5b07f39e73f4bfe6
anupkumarjana <janaanup7872@gmail.com>
2024-04-08 21:39:58 +0530

    renamed the project

--- practise/src/App.js
+++ practise/src/App.js
@@ -1,27 +1,5 @@
-import "./App.css";
-import Header from "./components/Header";
-import ProductFilter from "./components/ProductFilter";
-import Products from "./components/Products";
-import { createBrowserRouter, Outlet } from "react-router-dom";
-
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Outlet />
-    </div>
-  );
+  return <div className="App"></div>;
 }
-
-export const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/product", element: <Products /> },
-      { path: "/login", element: <ProductFilter /> },
-    ],
-  },
-]);
 
 export default App;
